78740c52b12b0a2d552013b38e32272e1b6128ca
Anupam <129939294+KhareAnupam@users.noreply.github.com>
2025-07-14 12:22:37 +0530

    Time: 0 ms (100%), Space: 14.1 MB (38.44%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,26 +1,29 @@
 class Solution {
     public:
-    void finalc(int ind, int target, vector<int>& arr,vector<int>& ds,vector<vector<int>>& ans){
-            if(target ==0){
+    void fullcombination(int ind,vector<int>& arr,int target,vector<vector<int>> &ans,vector<int> &ds){
+        if(ind>= arr.size() || target==0){
+            if(target==0){
                 ans.push_back(ds);
                 return;
             }
-        for(int i=ind;i<arr.size();i++){
-            if(i>ind && arr[i]==arr[i-1]) continue;
-            if(arr[i]>target) break;
-            ds.push_back(arr[i]);
-            finalc(i+1,target-arr[i],arr,ds,ans);
-            ds.pop_back();
-        
+            else return;
         }
-        // finalc(ind+1,target,arr,ds,ans);
+
+        for(int i=ind;i<arr.size();i++){
+            if(i!=ind && arr[i]==arr[i-1]) continue;
+            if(arr[i]>target) continue;
+            ds.push_back(arr[i]);
+            fullcombination(i+1,arr,target-arr[i],ans,ds);
+            ds.pop_back();
+        }
     }
+
 public:
     vector<vector<int>> combinationSum2(vector<int>& arr, int target) {
         sort(arr.begin(),arr.end());
         vector<vector<int>> ans;
         vector<int> ds;
-        finalc(0,target,arr,ds,ans);
+        fullcombination(0,arr, target,ans,ds);
         return ans;
     }
 };
